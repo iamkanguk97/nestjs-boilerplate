4763256cb2975dd2008359b11c8372e00edaf8e9
iamkanguk97 <iamkanguk97@kakao.com>
2025-03-27 18:07:38 +0900

    feat(#7): apply typeorm (need some feedback)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './apis/users/models/user';
 import { UserModule } from './apis/users/user.module';
+import { TypeOrmConfigService } from './database/typeorm-config.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'hellopassword',
-      database: 'nestjs-boilerplate-db',
-      entities: [User],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
     }),
     UserModule,
   ],
